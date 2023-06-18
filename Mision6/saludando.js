de18c6d8d48f54d5cac7c3f5e860418cfefb03b4
Maria Paula Rojas Henao <134750159+Mapu10@users.noreply.github.com>
2023-06-17 20:33:57 -0500

    Create saludando.js

---
+++ Mision6/saludando.js
@@ -0,0 +1,10 @@
+let time = 10;
+if (time < 12){
+  console.log("BUENOS DIAS") ;
+}
+else if (time >= 12 && time <= 18){
+  console.log("BUENAS TARDES") ;
+}
+else {
+  console.log("BUENAS NOCHES") ;
+}

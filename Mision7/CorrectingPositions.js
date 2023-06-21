ad26eec3edf9691e8d5bcab27a1d6af4013623e5
Maria Paula <mapurojas@gmail.com>
2023-06-21 18:30:10 -0500

    Corrección Eje 3 Mision 7

--- Mision7/CorrectingPositions.js
+++ Mision7/CorrectingPositions.js
@@ -1,1 +1,9 @@
 let array = ["Primero", "Segundo", "Tercero", "Cuarto", "Soy el último elemento"]; 
+
+array.splice(4, 0, "Quinto", "Sexto")
+
+for (let i = 0; i < array.lenght; i++) {
+    console.log(i + array[i]);
+}
+
+console.log(array)
